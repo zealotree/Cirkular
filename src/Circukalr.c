b84cb81847df96df2a2a3111e8746106f732fad7
Anonymous <Anonymous>
2016-07-08 05:27:56 -0400

    Adjusted date and day to start a 0 index; Adjusted marks by 5

--- src/Circukalr.c
+++ src/Circukalr.c
@@ -55,15 +55,18 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 
   graphics_context_set_stroke_color(ctx, GColorRed);
 
-  for(int i = 0; i < 12; i++) {
+  for(int i = 0; i < 13; i++) {
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
+//     APP_LOG(APP_LOG_LEVEL_DEBUG, "month is %d", s_last_time.month);
+
     int hour_angle = get_angle_for_hour(i);
     graphics_context_set_text_color(ctx, GColorWhite);
 
     GPoint pos = gpoint_from_polar(month_hour_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
     graphics_context_set_fill_color(ctx, GColorBlack);
-    if (i == s_last_time.month) {
+    if (i == s_last_time.month + 1) {
       
-      if (s_last_time.h12 == s_last_time.month) {
+      if (s_last_time.h12 == s_last_time.month + 1) {
         graphics_context_set_stroke_color(ctx,GColorFolly);
         graphics_context_set_fill_color(ctx, GColorLightGray);
         graphics_context_set_stroke_width(ctx, 2);
@@ -99,29 +102,27 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   
   GRect day_ring = grect_inset(bounds, GEdgeInsets(47));
   for(int i = 1; i < 31; i++) {
-    int ri;
-//     APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
 
     int day_angle = get_angle_for_day(i);
     graphics_context_set_text_color(ctx, GColorWhite);
     
-    if (s_last_time.day == 1){
-      ri = 30;
-    } else if (s_last_time.day == 31) {
-      ri = 31;
-    } else {
-      ri = s_last_time.day - 1; // debug here
-    }
+//     if (s_last_time.day == 1){
+//       ri = 30;
+//     } else if (s_last_time.day == 31) {
+//       ri = 31;
+//     } else {
+//       ri = s_last_time.day - 1; // debug here
+//     }
 //     APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", ri);
 
-    int v = i + 1;
+//     int v = i + 1;
 
     GPoint pos = gpoint_from_polar(day_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(day_angle));
-    if (i == ri) {
+    if (i == s_last_time.day) {
       graphics_context_set_fill_color(ctx, GColorBlack);
       graphics_fill_circle(ctx, pos, 3);
       
-      if (v % 4 == 0 || i == ri || i == 30){
+      if (i % 5 == 0){
         graphics_context_set_stroke_width(ctx, 1);
         graphics_context_set_stroke_color(ctx, GColorBlack);
       } else {
@@ -130,7 +131,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
       }
       graphics_draw_circle(ctx, pos, 3);
     } else {
-      if (v % 4 == 0 || i == ri || i == 30){
+      if (i % 5 == 0){
         graphics_context_set_stroke_width(ctx, 1);
         graphics_context_set_stroke_color(ctx, GColorBlack);
       } else {
@@ -142,7 +143,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
     
     if (i == 30) {
       GPoint pos_31 = GPoint(pos.x, pos.y + 8);
-      if (ri == 31) {
+      if (s_last_time.day == 31) {
         graphics_context_set_stroke_width(ctx, 1);
         graphics_context_set_stroke_color(ctx, GColorBlack);
         graphics_context_set_fill_color(ctx, GColorBlack);
@@ -180,7 +181,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 
   
   GRect week_ring = grect_inset(bounds, GEdgeInsets(69));
-  for(int i = 0; i < 8; i++) {
+  for(int i = 0; i < 7; i++) {
     int dow_angle = get_angle_for_weekday(i);
     GPoint pos = gpoint_from_polar(week_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(dow_angle));
     graphics_context_set_stroke_color(ctx, GColorDarkGray);
@@ -207,27 +208,17 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
   }
   graphics_draw_text(ctx, ap, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS), pm_box,
                      GTextOverflowModeWordWrap, GTextAlignmentCenter, s_attributes);
-
-  // Set the fill color
-//   graphics_context_set_fill_color(ctx, GColorBlue);
-  
-//   GPoint start = GPoint(50, 10);
-//   GPoint end = GPoint(60, 60);
-//   graphics_draw_line(ctx, start, end);
 }
 static void tick_handler(struct tm *tick_time, TimeUnits changed) {
+
   
-  
-	s_last_time.h24 = tick_time->tm_hour;
-  if (tick_time->tm_hour > 12) {
+  s_last_time.h24 = tick_time->tm_hour;
+    if (tick_time->tm_hour > 12) {
     s_last_time.h12 = s_last_time.h24 - 12;
   } else {
     s_last_time.h12 = tick_time->tm_hour;
   }
-  
-//   s_last_time.minutes = tick_time->tm_min;
   strftime(m_buffer, sizeof(m_buffer), "%M", tick_time);
-  
   s_last_time.month = tick_time->tm_mon;
   s_last_time.week_day = tick_time->tm_wday;
   s_last_time.day = tick_time->tm_mday;
@@ -238,6 +229,9 @@ static void tick_handler(struct tm *tick_time, TimeUnits changed) {
   } else {
     s_last_time.ap = 2;
   }
+
+
+
 
   layer_mark_dirty(draw_layer);
 }
@@ -264,6 +258,7 @@ void handle_init(void) {
 void handle_deinit(void) {
   window_destroy(my_window);
   layer_destroy(draw_layer);
+  graphics_text_attributes_destroy(s_attributes);
 }
 
 int main(void) {
