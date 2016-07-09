97a8180db32951b0bc87bcc671d9cff3bc64d61e
Anonymous <Anonymous>
2016-07-09 19:38:02 -0400

    fix

--- src/Circukalr.c
+++ src/Circukalr.c
@@ -84,81 +84,20 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 
   for(int i = 0; i < 13; i++) {
     //APP_LOG(APP_LOG_LEVEL_DEBUG, "%d", i);
-//     APP_LOG(APP_LOG_LEVEL_DEBUG, "month is %d", s_last_time.month);
-//
-    int hour_angle = get_angle_for_hour(i);
+  int hour_angle = get_angle_for_hour(i);
 
-    GPoint pos = gpoint_from_polar(month_hour_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
+  GPoint pos = gpoint_from_polar(month_hour_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
 
-    // if (persist_read_int(SUNRISE_KEY)) {
-    //   int vv;
-    //   int vap;
-    //   time_t sunrise_t =   persist_read_int(SUNRISE_KEY);
-    //   struct tm *sunrise_time = localtime(&sunrise_t);
-    //   if (sunrise_time->tm_hour > 12) {
-    //     vv = sunrise_time->tm_hour - 12;
-    //   } else {
-    //     vv = sunrise_time->tm_hour;
-    //   }
-    //   strftime(ap_buffer, sizeof(ap_buffer), "%p", sunrise_time);
-    //   if (! strcmp(ap_buffer, "AM")) {
-    //     vap = 1;
-    //   } else {
-    //     vap = 2;
-    //   }
-    //   if (vv == i && vap == s_last_time.ap && 
-    //       i != s_last_time.month && vv != s_last_time.h12) {
-    //     // Just outline if nothing conflicts
-    //     graphics_context_set_fill_color(ctx, theme.SunriseFillBg);
-    //     graphics_fill_circle(ctx, pos, 16);   
-    //   } else if (vv == i && vap == s_last_time.ap 
-    //       && i == s_last_time.month && s_last_time.h12 != vv) {
-    //     // sunrise is on the same marker as the month but not hour
-    //     // Just draw
-    //     graphics_context_set_stroke_width(ctx, 2);
-    //     graphics_context_set_stroke_color(ctx, theme.SunriseOutlineFg);
-    //     graphics_draw_circle(ctx, pos, 16);   
-    //   } else if (vv == i && vap == s_last_time.ap 
-    //       && i == s_last_time.month && s_last_time.h12 != vv) {
-    //     // same month, and hour, just do nothing
-    //   }
-    // }
-
-   	if (persist_read_int(SUNRISE_KEY)) {
+  if (persist_read_int(SUNRISE_KEY)) {
 		int h12;
 		int temp_ap;
-		time_t sunrise_t =   persist_read_int(SUNRISE_KEY);
-		struct tm *sunrise_time = localtime(&sunrise_t);
-
-		if (sunrise_time->tm_hour > 12) {
-		h12 = sunrise_time->tm_hour - 12;
-		} else {
-		h12 = sunrise_time->tm_hour;
-		}
-
-		strftime(ap_buffer, sizeof(ap_buffer), "%p", sunrise_time);
-		if (! strcmp(ap_buffer, "AM")) {
-			temp_ap = 1;
-		} else {
-			temp_ap = 2;
-		}
-
-		if (i == h12 && temp_ap == s_last_time.ap) {
-		  graphics_context_set_fill_color(ctx, theme.SunriseFillBg);
-		  graphics_fill_circle(ctx, pos, 15); 
-		}
-    }
-
-   	if (persist_read_int(SUNRISE_KEY)) {
-		int h12;
-		int temp_ap;
-		time_t sunset_t =   persist_read_int(SUNRISE_KEY);
+		time_t sunset_t = persist_read_int(SUNRISE_KEY);
 		struct tm *sunset_time = localtime(&sunset_t);
 
 		if (sunset_time->tm_hour > 12) {
-		h12 = sunset_time->tm_hour - 12;
-		} else {
-		h12 = sunset_time->tm_hour;
+      h12 = sunset_time->tm_hour - 12;
+      } else {
+      h12 = sunset_time->tm_hour;
 		}
 
 		strftime(ap_buffer, sizeof(ap_buffer), "%p", sunset_time);
@@ -172,18 +111,18 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 		  graphics_context_set_fill_color(ctx, theme.SunsetFillBg);
 		  graphics_fill_circle(ctx, pos, 15); 
 		}
-    }
+  }
 
 	if (persist_read_int(SUNSET_KEY)) {
 		int h12;
 		int temp_ap;
-		time_t sunset_t =   persist_read_int(SUNSET_KEY);
+		time_t sunset_t = persist_read_int(SUNSET_KEY);
 		struct tm *sunset_time = localtime(&sunset_t);
 
 		if (sunset_time->tm_hour > 12) {
-		h12 = sunset_time->tm_hour - 12;
-		} else {
-		h12 = sunset_time->tm_hour;
+      h12 = sunset_time->tm_hour - 12;
+      } else {
+      h12 = sunset_time->tm_hour;
 		}
 
 		strftime(ap_buffer, sizeof(ap_buffer), "%p", sunset_time);
@@ -193,13 +132,12 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
 			temp_ap = 2;
 		}
 
-		if (i == h12 && temp_ap == s_last_time.ap) {
+		if (i == h12 && temp_ap == s_last_time.ap) { // Only show either Sunrise or Sunset
 		  graphics_context_set_fill_color(ctx, theme.SunsetFillBg);
 		  graphics_fill_circle(ctx, pos, 15); 
 		}
-    }
+  }
 
-    // GPoint pos = gpoint_from_polar(month_hour_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle));
 
 	if (i == s_last_time.h12) { // Mark the current hour
       graphics_context_set_fill_color(ctx, theme.CurrentHourBg);
@@ -207,7 +145,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
       graphics_context_set_stroke_width(ctx, 1);
       graphics_fill_circle(ctx, pos, 14);
       graphics_draw_circle(ctx, pos, 15);
-	  graphics_context_set_text_color(ctx, theme.CurrentMinuteFg);
+      graphics_context_set_text_color(ctx, theme.CurrentMinuteFg);
       GRect kte = grect_centered_from_polar(month_hour_ring, GOvalScaleModeFitCircle, DEG_TO_TRIGANGLE(hour_angle), GSize(26,26));
       graphics_draw_text(ctx, m_buffer+((' ' == m_buffer[0])?1:0), fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS), kte,
                      GTextOverflowModeWordWrap, GTextAlignmentCenter, s_attributes);
@@ -219,7 +157,7 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
       graphics_fill_circle(ctx, pos, 14);
     } else {
       graphics_context_set_stroke_color(ctx, theme.OuterRingFg);
-	  graphics_context_set_fill_color(ctx, theme.OuterRingEmptyBg);
+      graphics_context_set_fill_color(ctx, theme.OuterRingEmptyBg);
       graphics_context_set_stroke_width(ctx, 1);
       graphics_draw_circle(ctx, pos, 15);  
       graphics_fill_circle(ctx, pos, 14);
@@ -250,13 +188,13 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
       graphics_draw_circle(ctx, pos, 3);
     } else if (i % 5 == 0) { // Mark the fives
       graphics_context_set_stroke_color(ctx, theme.MiddleRingMarkerFg);
-	  graphics_context_set_fill_color(ctx, theme.MiddleRingEmptyBg);
+      graphics_context_set_fill_color(ctx, theme.MiddleRingEmptyBg);
       graphics_context_set_stroke_width(ctx, 1);
       graphics_fill_circle(ctx, pos, 3);
       graphics_draw_circle(ctx, pos, 3);  
     } else { // Just fill
       graphics_context_set_stroke_color(ctx, theme.MiddleRingFg);
-	  graphics_context_set_fill_color(ctx, theme.MiddleRingEmptyBg);
+      graphics_context_set_fill_color(ctx, theme.MiddleRingEmptyBg);
       graphics_context_set_stroke_width(ctx, 1);
       graphics_fill_circle(ctx, pos, 3);
       graphics_draw_circle(ctx, pos, 3);  
@@ -273,8 +211,8 @@ static void canvas_update_proc(Layer *layer, GContext *ctx) {
         graphics_context_set_stroke_width(ctx, 1);
         graphics_context_set_stroke_color(ctx, theme.MiddleRingFg);
         graphics_context_set_fill_color(ctx, theme.MiddleRingEmptyBg);
-		graphics_fill_circle(ctx, pos_31, 3);
-		graphics_draw_circle(ctx, pos_31, 3);  
+        graphics_fill_circle(ctx, pos_31, 3);
+        graphics_draw_circle(ctx, pos_31, 3);  
       }
     }
   }
